d3e1e9e17a5cd970e2b10aaca250a107f294719e
Romeo-Perl-14 <romeoperlstein14@gmail.com>
2023-02-01 20:05:47 -0500

    fixed A8e

--- MATH246/HW1_A11.m
+++ MATH246/HW1_A11.m
@@ -1,16 +1,17 @@
 %%%%%%%% Solving for multiple equations at once, and more! %%%%%%%%
-%% Romeo Perlstein %%
+%%%%%%%% Romeo Perlstein %%%%%%%%
 
+%%%% setup the equations for use %%%%
 syms xvar yvar;
 equn1 = (xvar^2) - yvar^2;
 equn2 = (2*xvar) + yvar;
 ans1 = 1;
 ans2 = 2;
 
-%% Output answer %%
-[xAns, yAns] = solve([equn1 == ans1, equn2 == ans2]);
+%%%% Output answer %%%%
+[xAns, yAns] = solve([equn1 == ans1, equn2 == ans2])
 
-%% Now work to plot the functions %%
+%%%% Now work to plot the functions %%%%
 
 func1 = -(2*xvar) + 2;
 func2 = sqrt((xvar^2-1));
@@ -21,15 +22,17 @@ yAns_real = double(yAns);
 
 
 
-
+title('Plot of first and second functions, as well as x and y intercepts');
+xlabel('x-axis');
+ylabel('y-axis');
 hold on
-fplot(func1, [-5 5]);
-fplot(func2, [-5 5]);
-fplot(func3, [-5 5]);
+fplot(func1, [-5 5], "-r");
+fplot(func2, [-5 5], "-b");
+fplot(func3, [-5 5], "-b");
 
-xline(xAns_real);
-yline(yAns_real);
+xline(xAns_real, "-g");
+yline(yAns_real, "-m");
 
-%% EZ PZ %%
+%%%% EZ PZ %%%%
 
 
--- MATH246/HW1_A8e.m
+++ MATH246/HW1_A8e.m
@@ -1,12 +1,23 @@
 %%%%%% Computing Integrals %%%%%%
-%% Romeo Perlstein %%
+%%%%%% Romeo Perlstein %%%%%%
 
+%%%% declare syms variables %%%%
 syms x;
 upper_bound = 1;
 lower_bound = 0;
 
-func = sqrt(x/(1-x));
+%%%% declare the functions for use %%%%
+func = sqrt(1+(x^4));
+func_numEval = @(x) sqrt(1+(x.^4));
 
-int(func, lower_bound, upper_bound)
+%%%% solve the functions and save them into variables... because... %%%%
+disp(' ')
+disp('------ regular integration ------')
+regular_int = int(func, lower_bound, upper_bound)
+disp('------ solving the hypergeom function ------')
+vpa(regular_int)
+
+disp('------ numerical integration ------')
+numerical_int = integral(func_numEval, lower_bound, upper_bound)
 
 %%%% EZ PZ %%%%
