9999429c607e81b8c1fa40f66cf8cfb405dbcd51
Romeo-Perl-14 <romeoperlstein14@gmail.com>
2023-05-04 14:23:30 -0400

    finished the first problem... it's so over

--- MATH246/HW4_E14.m
+++ MATH246/HW4_E14.m
@@ -2,7 +2,8 @@
 
 %%%% Useful notation:
 %%%% heaviside function u(t), u_c(t-c)
-
+close all
+close all force
 %% Question E14
 
 %%% a)
@@ -24,22 +25,50 @@ og_vspace_eq_A = ilaplace(solved_laplace_eq_A) % back to the original vector spa
 
 %%% b)
 % y" + 6y' + 8y = h(t)
-% syms h(t)
-% h = piecewise(0<=t<5,0, 5<=t<10,1, t>=10,-1) % create the piecewise expression
-% og_eq_B = diff(y, 2) + 6*diff(y, 1) + 8*y == h % the OG equation
-% laplace_eq_B = laplace(og_eq_B, t, s) % Laplace transform of eq B
-% 
-% % even though I believe the previous step was unecessary, I will be leaving
-% % it in
-% better_laplace_eq_B = subs(og_eq_B, [y(0), subs(diff(y(t), t), t, 0), laplace(y(t), t, s)], [0, 2, Y])
-% solve_laplace_eq_B = simplify(solve(better_laplace_eq_A, Y))
-% og_vspace_eq_B = ilaplace(solve_laplace_eq_B)
+syms hB yB(tB) YB sB
+hB = heaviside(tB-5) + heaviside(tB-10)*(1-0) + heaviside(tB-10)*(-1-2) % the piecewise equation
+og_eq_B = diff(yB, 2) + 6*diff(yB, 1) + 8*yB == hB % the OG equation
+laplace_eq_B = laplace(og_eq_B, tB, sB) % Laplace transform of eq B
 
-fplot(og_vspace_eq_A, [0,15])
+% even though I believe the previous step was unecessary, I will be leaving
+% it in
+better_laplace_eq_B = subs(laplace_eq_B, [laplace(yB(tB), tB, sB), yB(0), subs(diff(yB(tB), tB), tB, 0)], [YB, 0, 2]) % different from equation A!
+solved_laplace_eq_B = (simplify(solve(better_laplace_eq_B, YB)))
+og_vspace_eq_B = ilaplace(solved_laplace_eq_B)
 
 
+%%% c)
+% y" + 4y = dirac(t-3pi), y(0)=1, y'(0)=0
+syms yC(tC) YC sC
+og_eq_C = diff(yC, 2) + 4*yC == dirac(tC - (3*pi)) %the original equation C
+laplace_eq_C = laplace(og_eq_C, tC, sC) % laplace transform of eq C
 
-% fplot(og_vspace_eq_A, [0, 15])
+better_laplace_eq_C = subs(laplace_eq_C, [laplace(yC(tC), tC, sC), yC(0), subs(diff(yC(tC), tC), tC, 0)], [YC, 1, 0])
+solved_laplace_eq_C = simplify(solve(better_laplace_eq_C, YC))
+og_vspace_eq_C = ilaplace(solved_laplace_eq_C)
+
+%%% d)
+% y" + y = driac(t-2) - dirac(t-8)
+syms yD(tD) YD sD
+og_eq_D = diff(yD, 2) + yD == (dirac(tD - 2)) - (dirac(tD - 8))
+laplace_eq_D = laplace(og_eq_D, tD, sD)
+
+better_laplace_eq_D = subs(laplace_eq_D, [laplace(yD(tD), tD, sD), yD(0), subs(diff(yD(tD), tD), tD, 0)], [YD, 0, 0])
+solved_laplace_eq_D = simplify(solve(better_laplace_eq_D, YD))
+og_vspace_eq_D = ilaplace(solved_laplace_eq_D)
+
+
+%% Plotting The Results
+%%% Here, I plot the results from A, B, C, and D on different intervals to show the full form of the grap 
+hold on
+fplot(og_vspace_eq_A, [-15,15])
+figure
+fplot(og_vspace_eq_B, [0, 15])
+figure
+fplot(og_vspace_eq_C, [-15, 30])
+figure
+fplot(og_vspace_eq_D, [-15, 15])
+
 
 
 
